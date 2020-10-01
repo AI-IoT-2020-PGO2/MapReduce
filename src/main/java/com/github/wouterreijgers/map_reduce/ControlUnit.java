6e41f2d73fb52f2550e381665cc6e8083d8b5df7
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-10-01 16:52:43 +0200

    Add todo list

--- src/main/java/com/github/wouterreijgers/map_reduce/ControlUnit.java
+++ src/main/java/com/github/wouterreijgers/map_reduce/ControlUnit.java
@@ -5,6 +5,9 @@ public class ControlUnit extends Thread {
     @Override
     public void run()
     {
-
+        // TODO Get elements from database (noSQL) and write them to a file
+        // TODO Start map reducing and pass the path to the previous file
+        // TODO Read entries from written file
+        // TODO Write read entries to other database (SQL)
     }
 }
