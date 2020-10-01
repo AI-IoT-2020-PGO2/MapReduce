8a6d2533d479a2c131724585868e6a30d862bbaa
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-10-01 17:16:02 +0200

    Add temp MySQL statements

--- src/main/java/com/github/wouterreijgers/map_reduce/database/WriteDatabase.java
+++ src/main/java/com/github/wouterreijgers/map_reduce/database/WriteDatabase.java
@@ -1,4 +1,48 @@
 package com.github.wouterreijgers.map_reduce.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class WriteDatabase {
+
+    public void write() {
+        try
+        {
+            /*
+             * Connect to DB
+             */
+
+            // create a mysql database connection
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://localhost/test";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "");
+
+            // the mysql insert statement
+            String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)"
+                    + " values (?, ?, ?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setString (1, "Barney");
+            preparedStmt.setString (2, "Rubble");
+            preparedStmt.setBoolean(4, false);
+            preparedStmt.setInt    (5, 5000);
+
+            // execute the preparedstatement
+            preparedStmt.execute();
+
+            /*
+             * Close connection
+             */
+
+            conn.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Got an exception!");
+            System.err.println(e.getMessage());
+        }
+    }
 }
