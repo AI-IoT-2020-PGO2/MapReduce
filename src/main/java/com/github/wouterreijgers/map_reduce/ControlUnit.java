1501c3a092e2e1948924b719a3753631ab128642
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-10-01 17:05:40 +0200

    Add loop

--- src/main/java/com/github/wouterreijgers/map_reduce/ControlUnit.java
+++ src/main/java/com/github/wouterreijgers/map_reduce/ControlUnit.java
@@ -5,9 +5,23 @@ public class ControlUnit extends Thread {
     @Override
     public void run()
     {
-        // TODO Get elements from database (noSQL) and write them to a file
-        // TODO Start map reducing and pass the path to the previous file
-        // TODO Read entries from written file
-        // TODO Write read entries to other database (SQL)
+        boolean isRunning = true;
+        while (isRunning)
+        {
+            // TODO Get elements from database (noSQL) and write them to a file
+            // TODO Start map reducing and pass the path to the previous file
+            // TODO Read entries from written file
+            // TODO Write read entries to other database (SQL)
+            try
+            {
+                Thread.sleep(300000);
+            }
+            catch (InterruptedException e)
+            {
+                isRunning = false;
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Halted map reducing");
     }
 }
