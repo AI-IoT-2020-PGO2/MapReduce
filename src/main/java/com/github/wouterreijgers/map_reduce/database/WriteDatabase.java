431d35fadd8bbc636faa9ce909c287e95eb56fe7
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-10-01 17:55:50 +0200

    Update MySQL handling

--- src/main/java/com/github/wouterreijgers/map_reduce/database/WriteDatabase.java
+++ src/main/java/com/github/wouterreijgers/map_reduce/database/WriteDatabase.java
@@ -12,11 +12,19 @@ public class WriteDatabase {
     public void updateusers(Map<Integer, Integer> users) {
         int i = 0;
         Connection dbConnection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/club_iot","root","");
+            System.out.println("Connected");
+        }catch (SQLException | ClassNotFoundException e){
+            System.err.println(e);
+        }
+
         PreparedStatement preparedStatement = null;
         String updateTableSQL = "UPDATE users SET total_votes =? WHERE ID =? ";
         try
         {
-            //dbConnection = getDBConnection();
             for (Map.Entry<Integer, Integer> e : users.entrySet()){
                 preparedStatement = dbConnection.prepareStatement(updateTableSQL);
                 preparedStatement.setInt(1, e.getValue());
@@ -24,7 +32,7 @@ public class WriteDatabase {
                 preparedStatement .executeUpdate();
                 i++;
             }
-            dbConnection.commit();
+//            dbConnection.commit();
             System.out.println(i + " Records updated!");
         }
         catch (SQLException e) {
