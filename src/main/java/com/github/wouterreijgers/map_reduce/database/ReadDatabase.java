30f6edfde29c0742a23612510c9efb0c06cd9855
Wouter Reijgersberg <wouter.reijgersberg@student.uantwerpen.be>
2020-10-01 14:45:01 +0200

    Added dependencies

--- src/main/java/com/github/wouterreijgers/map_reduce/database/ReadDatabase.java
+++ src/main/java/com/github/wouterreijgers/map_reduce/database/ReadDatabase.java
@@ -1,4 +1,13 @@
 package com.github.wouterreijgers.map_reduce.database;
 
+/**
+ * This class will read a mongoDB database
+ */
+
 public class ReadDatabase {
+
+    public void setUp(){
+
+    }
+
 }
